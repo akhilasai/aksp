13efea2c31f941c39c4148412788765d0aa487e5
Akhila Sai Thumma <athumma@innominds.com>
2021-11-08 13:12:56 +0530

    created components

---
+++ front-end/client/src/components/bodycomponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Body(){
+   return( 
+       <>
+       <h1 >It is body component</h1>
+       </>
+   )
+}
---
+++ front-end/client/src/components/headercomponent.js
@@ -0,0 +1,8 @@
+import React from 'react';
+export default function Header(){
+   return( 
+       <>
+       <h1 style={{color:"orange"}}>It is header component</h1>
+       </>
+   )
+}
